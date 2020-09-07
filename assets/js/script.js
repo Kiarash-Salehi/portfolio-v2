a7fc7bc13f3eb6f44475383bdd9dba8ebd0e82e7
KSDragon82 <www.kiarashs2929@gmail.com>
2020-09-07 16:46:09 +0430

    added some pictures

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // document.getElementById('year').innerHTML = year;
 const nav = document.querySelector('nav');
 const blurDiv = document.querySelector('span.blur');
-window.addEventListener('scroll', e => {
+window.addEventListener('scroll', (e) => {
 	if (window.scrollY > 100) nav.style.backgroundColor = 'rgba(255, 255, 255, .7)';
 	else nav.style.backgroundColor = 'transparent';
 });
@@ -15,12 +15,13 @@ menuBtn.addEventListener('click', () => {
 	menuBtn.classList.toggle('open');
 	navItems.classList.toggle('open');
 	blurDiv.classList.toggle('open');
-	navItem.forEach(item => {
+	navItem.forEach((item) => {
 		item.addEventListener('click', () => {
 			menuBtn.classList.remove('open');
 			navItems.classList.remove('open');
 			blurDiv.classList.remove('open');
-	});});
+		});
+	});
 });
 
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
@@ -31,15 +32,14 @@ timeline
 	.from('main .header > div', { y: '50%', opacity: 0 }, 0);
 
 const floatingBtn = document.querySelector('main .header div .floating');
-floatingBtn.addEventListener('click', e => {
+floatingBtn.addEventListener('click', (e) => {
 	window.scrollTo(0, window.innerHeight);
 });
 
 const icon = document.querySelector('nav .nav__icon');
-icon.addEventListener('click', e => {
+icon.addEventListener('click', (e) => {
 	window.scrollTo(0, 0);
 });
-
 
 const react = document.getElementById('react');
 const node = document.getElementById('node');
@@ -49,16 +49,41 @@ const filters = document.querySelectorAll('.category');
 
 const projects = document.querySelectorAll('.project');
 
-filters.forEach(filter => {
+filters.forEach((filter) => {
 	filter.addEventListener('click', () => {
-		filters.forEach(filter => { filter.classList.remove('active'); });
+		filters.forEach((filter) => {
+			filter.classList.remove('active');
+		});
 		filter.classList.add('active');
 		let values = filter.innerText.toLowerCase();
-		projects.forEach(project => {
+		projects.forEach((project) => {
 			project.style.display = 'none';
-			if (project.getAttribute('data-skill') === values || project.getAttribute('data-skill2') === values || values === 'all') {
+			if (
+				project.getAttribute('data-skill') === values ||
+				project.getAttribute('data-skill2') === values ||
+				values === 'all'
+			) {
 				project.style.display = 'flex';
 			}
+		});
+	});
+});
+
+const sideImages = document.querySelectorAll('.side_images img');
+const travelLog = document.querySelector('#travel-log');
+const amazonClone = document.querySelector('#amazon-clone');
+const realestate = document.querySelector('#realestate');
+const covidTracker = document.querySelector('#covid-tracker');
+const netflixCone = document.querySelector('#netflix-clone');
+const yelpClone = document.querySelector('#yelp-clone');
+const projectImages = [travelLog, amazonClone, realestate, covidTracker, netflixCone, yelpClone];
+sideImages.forEach((img) => {
+	img.addEventListener('click', (e) => {
+		const biggerImageSrc = e.target.getAttribute('src');
+		const biggerImageid = e.target.getAttribute('data-bigger');
+		projectImages.forEach(projectImg => {
+			const projectImgId = projectImg.getAttribute('id');
+			if (biggerImageid === projectImgId) projectImg.setAttribute('src', biggerImageSrc);
 		});
 	});
 });
