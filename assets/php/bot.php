d481888242d5e5f6e3f87e737f39210557ff2dee
KSDragon82 <www.kiarashs2929@gmail.com>
2020-09-08 19:22:49 +0430

    testing sending email using php

---
+++ assets/php/bot.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($submit)) {
+	$to = 'skiarash57@gmail.com';
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$messageIn = $_POST['message'];
+	$email = $_POST['email'];
+	$message = "\n".$subject."\n".$messageIn."\n";
+
+	$headers = "From: ".$name." - ".$email."\r\n";
+	$headers .= "Reply-To: ".$email."\r\n";
+	$headers .= "Content-type: text/html\r\n";
+
+	mail($to, $subject, $message, $headers);
+	header("Location: ../../index.html?mailsend");
+}
